c84dd9197325ba1f615800d1ef492672aa99d10e
Manav Ahuja <48991969+manavahuja30@users.noreply.github.com>
2025-10-04 10:59:27 +0000

    Variables(Const,let and var)

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12435
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyd"
+let accountState
+
+// accountId = 2 //not allowed
+accountEmail = "a@b.com"
+accountPassword = "21334"
+accountCity = "Banglore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+console.log(accountId);
